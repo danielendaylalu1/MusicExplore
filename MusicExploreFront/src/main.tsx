bd3835763326d7d558f0d7619718a56e91c040a1
daniel endaylalu <endaylaludaniel@gmail.com>
2024-02-03 15:27:38 +0300

    added routes to frontend

--- MusicExploreFront/src/main.tsx
+++ MusicExploreFront/src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "albums",
+        element: <div>albums</div>,
+      },
+      {
+        path: "artists",
+        element: <div>artists</div>,
+      },
+      {
+        path: "genres",
+        element: <div>genres</div>,
+      },
+      {
+        path: "addsong",
+        element: <div>add</div>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
